66f2d377a7aa17011cd1e19fdbbe6fe4e38d284d
OhHaneol <otyvs1109@hs.ac.kr>
2024-06-11 01:49:10 +0900

    fix: remove @Test

--- src/test/java/study/NumberBaseballGameTest.java
+++ src/test/java/study/NumberBaseballGameTest.java
@@ -13,7 +13,6 @@ class NumberBaseballGameTest {
 
     static final NumberBaseballGame nbg = new NumberBaseballGame();
 
-    @Test
     @DisplayName("스트라이크 검증")
     @CsvSource(value = {"123:321:false:1", "123:123:true:3", "123:456:false:0", "123:124:false:2"}, delimiter = ':')
     @ParameterizedTest(name = "[{index}] 추측 {0}과 목표 {1}에 대해: 스트라이크 개수는 {3}, 게임 성공 여부는 {2}")
