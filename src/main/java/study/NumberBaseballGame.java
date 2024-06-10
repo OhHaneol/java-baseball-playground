98434b47add9a0eeae81e10b91e2a7ed78acde7c
OhHaneol <otyvs1109@hs.ac.kr>
2024-06-10 11:27:47 +0900

    feat: add resetGame

--- src/main/java/study/NumberBaseballGame.java
+++ src/main/java/study/NumberBaseballGame.java
@@ -1,7 +1,21 @@
 package study;
 
+import java.util.Scanner;
+
 public class NumberBaseballGame {
     public static void main(String[] args) {
+
+
+        int i;
+        do {
+            numberBaseballGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            Scanner sc = new Scanner(System.in);
+            i = sc.nextInt();
+        } while (i != 2);
+    }
+
+    static void numberBaseballGame() {
 
     }
 }
