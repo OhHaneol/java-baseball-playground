88140520919ad92d12c38fc2ea432ceadf477735
OhHaneol <otyvs1109@hs.ac.kr>
2024-06-10 13:19:27 +0900

    feat: add numberBaseballGame method

--- src/main/java/study/NumberBaseballGame.java
+++ src/main/java/study/NumberBaseballGame.java
@@ -1,21 +1,44 @@
 package study;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumberBaseballGame {
     public static void main(String[] args) {
 
-
-        int i;
+        int choice;
         do {
             numberBaseballGame();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             Scanner sc = new Scanner(System.in);
-            i = sc.nextInt();
-        } while (i != 2);
+            choice = sc.nextInt();
+        } while (choice != 2);
     }
 
     static void numberBaseballGame() {
+        Random random = new Random();
+        int target = random.nextInt(899) + 100; // 100 ~ 999
 
+        boolean is3strike;
+        do {
+            System.out.println("숫자를 입력해 주세요 : ");
+            Scanner sc = new Scanner(System.in);
+            int guess = sc.nextInt();
+
+            is3strike = isStrike(guess, target);
+            isBall(guess, target);
+            isNothing(guess, target);
+        } while (!is3strike);
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    static boolean isStrike(int guess, int target) {
+    }
+
+    static void isBall(int guess, int target) {
+    }
+
+    static void isNothing(int guess, int target) {
     }
 }
