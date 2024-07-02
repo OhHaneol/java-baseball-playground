a8087537b4ce9b00afe35d066cd0abbc14bddf78
OhHaneol <otyvs1109@hs.ac.kr>
2024-07-02 21:05:33 +0900

    야구_PLAY_검증

--- src/main/java/applyFeedback/Balls.java
+++ src/main/java/applyFeedback/Balls.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Balls {
-    private final List<Ball> balls;
+    private final List<Ball> answers;
 
     public Balls(List<Integer> answer) {
-        this.balls = mapBall(answer);
+        this.answers = mapBall(answer);
     }
 
     private List<Ball> mapBall(List<Integer> answer) {
@@ -19,7 +19,7 @@ public class Balls {
     }
 
     public BallStatus play(Ball userBall) {
-        return balls.stream()
+        return answers.stream()
                 .map(answer -> answer.play(userBall))   // ball.play 는 BallStatus 를 반환한다. 따라서 컴퓨터의 ball 3개에 대해 userBall 을 play 한 BallStatus 3개를 반환한다.
 //                .filter(status -> status != BallStatus.NOTHING)
 //                .filter(status -> status.isNotNothing())
@@ -28,4 +28,13 @@ public class Balls {
                 .orElse(BallStatus.NOTHING);
     }
 
+    public PlayResult play(List<Integer> balls) {
+        Balls userBalls = new Balls(balls);
+        PlayResult result = new PlayResult();
+        for (Ball answer : answers) {
+            BallStatus status = userBalls.play(answer);
+            result.report(status);
+        }
+        return result;
+    }
 }
--- src/test/java/applyFeedback/BallsTest.java
+++ src/test/java/applyFeedback/BallsTest.java
@@ -8,6 +8,32 @@ import static org.assertj.core.api.Assertions.*;
 
 public class BallsTest {
     @Test
+    public void play_nothing() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(1, 2, 3));
+        assertThat(result.getStrike()).isEqualTo(0);
+        assertThat(result.getBall()).isEqualTo(0);
+    }
+
+    @Test
+    public void play_1strike_1ball() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(1, 4, 2));
+        assertThat(result.getStrike()).isEqualTo(1);
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @Test
+    public void play_3strike() {
+        Balls answers = new Balls(Arrays.asList(1, 2, 3));
+        PlayResult result = answers.play(Arrays.asList(1, 2, 3));
+        assertThat(result.getStrike()).isEqualTo(3);
+        assertThat(result.getBall()).isEqualTo(0);
+//        assertThat(result.getStrike() == 3).isTrue(); // 값을 꺼내는 게 아니라(상태에 직접 접근 X)
+        assertThat(result.isGameEnd()).isTrue();        // 메시지를 보내는 것이 객체지향
+    }
+
+    @Test
     public void nothing() {
         Balls answers = new Balls(Arrays.asList(1, 2, 3));
         BallStatus status = answers.play(new Ball(1, 4));
